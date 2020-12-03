f989dc4382758fda16561c6305508c36e743506d
Mavlon <mavlon.idc@mail.ru>
2020-12-03 23:31:59 +0500

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+const profile = {}
+for(i = 1; i <= 10; i++) {
+    console.log('Пользователь ' + i);
+    console.log(`Имя ${profile.name = prompt('Введите имя')}`);
+    console.log(`Возраст ${profile.age = +prompt('Введите имя')}`);
+}
